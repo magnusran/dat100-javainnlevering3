ad6595512a9df6ef79b79ca1372022bcc6b5f0aa
magnusran <186493@stud.hvl.no>
2024-11-13 16:31:45 +0100

    Oppgave 5 (valgfri)

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -17,12 +17,53 @@ import javax.swing.JOptionPane;
 
 public class LesBlogg {
 
-	private static String TEKST = "TEKST";
-	private static String BILDE = "BILDE";
+    private static String TEKST = "TEKST";
+    private static String BILDE = "BILDE";
 
-	public static Blogg les(String mappe, String filnavn) {
+    public static Blogg les(String mappe, String filnavn) {
+        try (Scanner scanner = new Scanner(new File(mappe + "/" + filnavn))) {
+            int antallInnlegg = Integer.parseInt(scanner.nextLine());
 
-		throw new UnsupportedOperationException(TODO.method());
+            Blogg blogg = new Blogg(antallInnlegg);
 
-	}
+            for (int i = 0; i < antallInnlegg; i++) {
+                Innlegg innlegg = null;
+
+                // Få type
+                String type = scanner.nextLine();
+
+                // Få id
+                int id = Integer.parseInt(scanner.nextLine());
+
+                // Få navn
+                String navn = scanner.nextLine();
+
+                // Få dato
+                String dato = scanner.nextLine();
+
+                // Få likes
+                int likes = Integer.parseInt(scanner.nextLine());
+
+                // Få tekst
+                String tekst = scanner.nextLine();
+
+                switch (type) {
+                    case "BILDE":
+                        // Få bilde-URL
+                        String url = scanner.nextLine();
+                        innlegg = new Bilde(id, navn, dato, likes, tekst, url);
+                        break;
+                    default:
+                        innlegg = new Tekst(id, navn, dato, likes, tekst);
+                        break;
+                }
+                blogg.leggTil(innlegg);
+            }
+
+            return blogg;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
